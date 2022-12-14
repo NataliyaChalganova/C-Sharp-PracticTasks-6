00e077923ae96cfb038e3c0ba80f11e5489ce862
Natasha <n.chalganova@mail.ru>
2022-12-14 11:25:53 +0300

    Task 47, 50, 52

---
+++ Task50/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк двумерного массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива:");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите искомое в массиве число: "); 
+int number=Convert.ToInt32 (Console.ReadLine());
+int [,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(CheckNumberIn(number, array));
+
+void FillArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+        for (int j=0;j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(1,10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],3}\t");
+        Console.WriteLine();  
+    }
+}
+
+{ 
+    foreach (int el in array)   
+    Console.WriteLine(); 
+} 
+bool CheckNumberIn(int number, int[,] array) 
+{ 
+    bool find = false; 
+    foreach (int el in array) 
+    { 
+        if (el == number) 
+        { 
+            find = true; 
+            break; 
+        } 
+    } 
+    return find; 
+} 
---
+++ Task52/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите число строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m,n];
+Random rnd = new Random();
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        array[i,j] = rnd.Next(2, 10);
+        Console.Write($"{array[i,j]}, ");
+    }
+    Console.WriteLine();
+}
+
+
+for(int i = 0; i < m; i++)
+{   
+    int result = 0;
+    for(int j = 0; j < n; j++)
+    {  
+       result += array[j,i]; 
+       Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)result/m}");
+    }
+}
